89a01df2b9091c81aa7b16e15fee93740f19c110
Fraser Doswell <doswellf@users.noreply.github.com>
2017-02-09 15:59:07 -0500

    removed javadoc and bad code

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -14,70 +14,22 @@ import java.util.List;
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
-
-    /**
-     * add
-     * @param entry
-     *
-     */
-    
     public void add(final TimeEntry entry) {
         entries.add(entry);
     }
 
-    /**
-     * add
-     * @param entry
-     *
-     */
     public void remove(final TimeEntry entry) {
         entries.remove(entry);
     }
 
-    /**
-     * add
-     * @param entry
-     *
-     */
     public int size() {
-        // forces always-true if condition and empty if block
-        /*        
-        if (true) {
-          // do nothing
-        }
-        */
         return entries.size();
     }
 
-    /**
-     * add
-     * @param index
-     * @return TimeEntry
-     */
     public TimeEntry get(final int index) {
-        
-        // forces empty try and catch block warnings
-        /*
-        try { 
-        
-        } catch (NullPointerException npx) {
-        
-        }
-        */
-        
         return entries.get(index);
     }
     
-    // forces a findbugs "might return null" warning
-/*    
-    @Override
-    public String toString() {
-        if (entries.size() < 1) 
-            return null;
-        else
-            return String.valueOf(entries.get(1));
-    }
-  */      
 }
 
 
